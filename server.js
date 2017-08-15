6bdf3c04d24596173cc6ffdccf62095511fd6e26
xy7313 <syrupyang@hotmail.com>
2017-08-14 23:56:07 -0400

    change repo name

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var app = express();
 var path = require('path');
 
 //middleware to define folder for strict files
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', function(req,res){
     res.sendFile(path.resolve(__dirname,'public','index.html'))
-})
+});
 
 app.listen(3000,function(){
     console.log('listening 3000');
-})
+});
 
--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
-"user strict"
+"use strict";
+import { createStore } from "redux";
+
+//step 3 define reducers in order to create the store
+//create reducer by passing two arguments, set the initial value for the state
+const reducer = function(state = 0, action) {
+  console.log("reducer...");
+  console.log("state: ", state, "action: ", action);
+  //the use of reducers are evaluate what to do
+  switch (action.type) {
+    case "INCREMENT":
+      return state + action.payload;
+      break;
+  }
+  return state;
+};
+
+//step 1 create the store
+const store = createStore(reducer);
+console.log("store: ", store);
+//see the current of store, use the subscript method
+store.subscribe(function() {
+  console.log("current state: " + store.getState());
+});
+
+//step 2 create and dispatch actions
+//an action is made by an object that has two properties
+store.dispatch({ type: "INCREMENT", payload: 1 });
+
+store.dispatch({ type: "INCREMENT", payload: 1 });
+
+store.dispatch({ type: "INCREMENT", payload: 1 });
+//type, key words in redux, payload, call it as you wish
