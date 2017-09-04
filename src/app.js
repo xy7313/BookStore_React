97fc550a7949d7f95dbb8622f5b12552611e98e7
xy7313 <syrupyang@hotmail.com>
2017-09-03 22:01:18 -0400

    restart change dispatch

--- src/app.js
+++ src/app.js
@@ -3,31 +3,34 @@ import { createStore } from "redux";
 
 //step 3 define reducers in order to create the store
 //create reducer by passing two arguments, set the initial value for the state
+//set initial value to state in function()
+//the use of reducers are evaluate what to do using switch
 const reducer = function(state = 0, action) {
-  console.log("reducer...");
-  console.log("state: ", state, "action: ", action);
-  //the use of reducers are evaluate what to do
+  console.log("reducer...state: ", state, "action: ", action);
   switch (action.type) {
     case "INCREMENT":
       return state + action.payload;
+      break;
+    case "decrement":
+      return state - action.payload;
       break;
   }
   return state;
 };
 
-//step 1 create the store
+//step 1 create the store, pass reducers as parameter
+//see the current of store, use the subscript method, add listener,
 const store = createStore(reducer);
 console.log("store: ", store);
-//see the current of store, use the subscript method
 store.subscribe(function() {
   console.log("current state: " + store.getState());
 });
 
 //step 2 create and dispatch actions
-//an action is made by an object that has two properties
-store.dispatch({ type: "INCREMENT", payload: 1 });
-
-store.dispatch({ type: "INCREMENT", payload: 1 });
-
-store.dispatch({ type: "INCREMENT", payload: 1 });
+//an action is made by an object that has two properties, type and payload
 //type, key words in redux, payload, call it as you wish
+store.dispatch({ type: "INCREMENT", payload: 1 });
+
+store.dispatch({ type: "INCREMENT", payload: 1 });
+
+store.dispatch({ type: "decrement", payload: 1 });
